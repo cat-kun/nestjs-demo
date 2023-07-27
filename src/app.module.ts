bc245edf9f48d3bf07d8868391c3888412a82b64
cat-kun <626269256@qq.com>
2023-07-27 17:31:09 +0800

    feat: utils

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GirlModule } from './girl/girl.module';
 import { LoginModule } from './login/login.module';
+import { JwtModule } from '@nestjs/jwt';
+import Utils from './utils';
 
 @Module({
-  imports: [GirlModule, LoginModule],
+  imports: [
+    GirlModule,
+    LoginModule,
+    JwtModule.register({
+      // 注入jwt模块
+      secret: 'dsh',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Utils],
 })
 export class AppModule {}
